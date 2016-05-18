ab42b042df3797efcb4baf76c330eec72fb5b082
Nguyen Nam <Nguyen Nam>
2016-05-18 18:58:18 +0700

    1

--- Cars/src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by MyComputer on 5/11/2016.
- */
-public class Main {
-    public static void main(String[] args) {
-        GameWindow gameWindow = new GameWindow();
-    }
-}
--- Cars/src/controller/BulletControllerManager.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-/**
- * Created by qhuydtvt on 5/6/2016.
- */
-public class BulletControllerManager extends ControllerManager {
-}
